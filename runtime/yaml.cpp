cbae514e774c882ed094bd7cdac25589a1119a0b
Nikolay Osintsev <niosintsev@miem.hse.ru>
2022-12-13 18:07:00 +0300

    add source files

---
+++ runtime/yaml.cpp
@@ -0,0 +1,94 @@
+#include <yaml-cpp/yaml.h>
+
+#include "runtime/optional.h"
+#include "runtime/streams.h"
+#include "runtime/string_functions.h"
+
+#include "runtime/yaml.h"
+
+void yaml_node_to_mixed(const YAML::Node &node, mixed &data) {
+  data.clear();
+  // TODO: just do it!
+}
+
+void mixed_to_yaml_node(const mixed &data, YAML::Node &node) {
+  if (!data.is_array()) {
+    if (data.is_null()) {
+      php_warning("Cannot convert (mixed)null into yaml node");
+      return;
+    }
+    node.push_back(data.as_string().c_str());
+    return;
+  } // TODO: for maps also
+  for (auto it = data.as_array().begin(); it != data.as_array().end(); ++it) {
+    mixed data_piece = it.get_value();
+    YAML::Node node_piece;
+    mixed_to_yaml_node(data_piece, node_piece);
+    node.push_back(node_piece);
+  }
+}
+
+bool f$yaml_emit_file(const string &filename, const mixed &data) {
+  if (filename.empty()) {
+    php_warning("Filename cannot be empty");
+    return false;
+  }
+  string emitted_data = f$yaml_emit(data);
+  Optional<int64_t> size = f$file_put_contents(filename, emitted_data);
+  if (size.is_false()) {
+    php_warning("Error while writing to file \"%s\"", filename.c_str());
+    return false;
+  }
+  return true;
+}
+
+string f$yaml_emit(const mixed &data) {
+  YAML::Node node;
+  mixed_to_yaml_node(data, node);
+//  YAML::Emitter out;
+//  out << node;
+//  if (!out.good()) {
+//    php_warning("Error while emitting to yaml");
+//    return string();
+//  }
+//  return string(out.c_str());
+  return string(YAML::Dump(node).c_str());
+}
+
+mixed f$yaml_parse_file(const string &filename, int pos) {
+  if (filename.empty()) {
+    php_warning("Filename cannot be empty");
+    return {};
+  }
+  Optional<string> data = f$file_get_contents(filename);
+  if (data.is_false()) {
+    php_warning("Error while reading file \"%s\"", filename.c_str());
+    return {};
+  }
+  return f$yaml_parse(data.ref(), pos);
+}
+
+mixed f$yaml_parse(const string &data, int pos) {
+//  if (pos != 0)
+//    php_warning("Value of pos other than 0 is not supported. Assigning 0 instead of %d", pos);
+  YAML::Node node;
+  if (pos > 0) {
+    Optional<string> optional_data = f$substr(data, pos);
+    if (optional_data.is_false()) {
+      php_warning("Error while making substring");
+      return {};
+    }
+    node = YAML::Load(optional_data.ref().c_str());
+  }
+  else if (pos < 0) {
+    php_warning("Argument \"pos\" cannot be negative, but it is %d. Setting to default (0)", pos);
+    node = YAML::Load(data.c_str());
+  }
+  else node = YAML::Load(data.c_str());
+//  std::string std_data(data.c_str());
+//  node = YAML::Load(data.c_str());
+//  node >> std_data;
+  mixed parsed_data;
+  yaml_node_to_mixed(node, parsed_data);
+  return parsed_data;
+}
