a12ca363dbaaa19c21e42507bb46010202f523dc
Nikolay Osintsev <niosintsev@miem.hse.ru>
2022-12-15 20:54:42 +0300

    yaml.cpp ready but crashes on not simple yaml files

--- runtime/yaml.cpp
+++ runtime/yaml.cpp
@@ -8,7 +8,28 @@
 
 void yaml_node_to_mixed(const YAML::Node &node, mixed &data) {
   data.clear();
-  // TODO: just do it!
+  if (node.IsNull()) {
+    php_warning("Yaml node is null");
+    return;
+  }
+  if (node.IsScalar()) {
+    data = string(node.as<std::string>().c_str());
+    return;
+  }
+  if (node.IsMap()) {
+    for (auto it = node.begin(); it != node.end(); ++it) {
+      mixed data_piece;
+      yaml_node_to_mixed(*it, data_piece);
+      data[string(it->first.as<std::string>().c_str())] = data_piece;
+    }
+  }
+  if (node.IsSequence()) {
+    for (auto it = node.begin(); it != node.end(); ++it) {
+      mixed data_piece;
+      yaml_node_to_mixed(*it, data_piece);
+      data.push_back(data_piece);
+    }
+  }
 }
 
 void mixed_to_yaml_node(const mixed &data, YAML::Node &node) {
@@ -17,14 +38,25 @@ void mixed_to_yaml_node(const mixed &data, YAML::Node &node) {
       php_warning("Cannot convert (mixed)null into yaml node");
       return;
     }
-    node.push_back(data.as_string().c_str());
+    node.push_back(std::string(data.as_string().c_str()));
     return;
-  } // TODO: for maps also
-  for (auto it = data.as_array().begin(); it != data.as_array().end(); ++it) {
-    mixed data_piece = it.get_value();
-    YAML::Node node_piece;
-    mixed_to_yaml_node(data_piece, node_piece);
-    node.push_back(node_piece);
+  }
+  array<mixed> data_array = data.as_array();
+  if (data_array.is_pseudo_vector()) {
+    for (auto it = data_array.begin(); it != data_array.end(); ++it) {
+      mixed data_piece = it.get_value();
+      YAML::Node node_piece;
+      mixed_to_yaml_node(data_piece, node_piece);
+      node.push_back(node_piece);
+    }
+  } else {
+    for (auto it = data_array.begin(); it != data_array.end(); ++it) {
+      mixed data_piece = it.get_value();
+      YAML::Node node_piece;
+      mixed_to_yaml_node(data_piece, node_piece);
+      node.force_insert(std::string(it.get_key().as_string().c_str()), node_piece);
+      //node[std::string(it.get_key().as_string().c_str())] = node_piece; // alternative way
+    }
   }
 }
 
@@ -69,8 +101,6 @@ mixed f$yaml_parse_file(const string &filename, int pos) {
 }
 
 mixed f$yaml_parse(const string &data, int pos) {
-//  if (pos != 0)
-//    php_warning("Value of pos other than 0 is not supported. Assigning 0 instead of %d", pos);
   YAML::Node node;
   if (pos > 0) {
     Optional<string> optional_data = f$substr(data, pos);
@@ -79,15 +109,12 @@ mixed f$yaml_parse(const string &data, int pos) {
       return {};
     }
     node = YAML::Load(optional_data.ref().c_str());
-  }
-  else if (pos < 0) {
-    php_warning("Argument \"pos\" cannot be negative, but it is %d. Setting to default (0)", pos);
+  } else {
+    if (pos < 0) {
+      php_warning("Argument \"pos\" cannot be negative, but it is %d. Setting to default (pos = 0)", pos);
+    }
     node = YAML::Load(data.c_str());
   }
-  else node = YAML::Load(data.c_str());
-//  std::string std_data(data.c_str());
-//  node = YAML::Load(data.c_str());
-//  node >> std_data;
   mixed parsed_data;
   yaml_node_to_mixed(node, parsed_data);
   return parsed_data;
